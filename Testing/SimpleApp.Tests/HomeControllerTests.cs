250b27a4345c388c1f827a68de46aa6a1f43f93d
Hinthusiast <68813392+Hinthusiast@users.noreply.github.com>
2021-09-08 13:14:55 +0800

    using Moq

--- Testing/SimpleApp.Tests/HomeControllerTests.cs
+++ Testing/SimpleApp.Tests/HomeControllerTests.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using SimpleApp.Controllers;
 using SimpleApp.Models;
 using Xunit;
+using Moq;
+
 namespace SimpleApp.Tests
 {
     public class HomeControllerTests
@@ -11,12 +13,16 @@ namespace SimpleApp.Tests
         public void IndexActionModelIsComplete()
         {
             // Arrange
-
+            // Arrange
+            Product[] testData = new Product[] {
+new Product { Name = "P1", Price = 75.10M },
+new Product { Name = "P2", Price = 120M },
+new Product { Name = "P3", Price = 110M }
+};
+            var mock = new Mock<IDataSource>();
+            mock.SetupGet(m => m.Products).Returns(testData);
             var controller = new HomeController();
-            Product[] products = new Product[] {
-                new Product { Name = "Kayak", Price = 275M },
-                new Product { Name = "Lifejacket", Price = 48.95M}
-            };
+
             // Act
             var model = (controller.Index() as ViewResult)?.ViewData.Model
             as IEnumerable<Product>;
@@ -24,6 +30,8 @@ namespace SimpleApp.Tests
             Assert.Equal(products, model,
             Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name
             && p1.Price == p2.Price));
+            mock.VerifyGet(m => m.Products, Times.Once);
+
         }
     }
     }
