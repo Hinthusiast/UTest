813bb22fcf77b6db6bf8a2d698ed54bb0ba06e08
Hinthusiast <68813392+Hinthusiast@users.noreply.github.com>
2021-09-08 13:19:28 +0800

    moq isolation instead of fake implementation

--- Testing/SimpleApp.Tests/HomeControllerTests.cs
+++ Testing/SimpleApp.Tests/HomeControllerTests.cs
@@ -27,9 +27,9 @@ new Product { Name = "P3", Price = 110M }
             var model = (controller.Index() as ViewResult)?.ViewData.Model
             as IEnumerable<Product>;
             // Assert
-            Assert.Equal(products, model,
-            Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name
-            && p1.Price == p2.Price));
+            Assert.Equal(testData, model,
+Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name
+&& p1.Price == p2.Price));
             mock.VerifyGet(m => m.Products, Times.Once);
 
         }
