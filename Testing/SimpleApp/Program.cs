f9b5ef361e6280e8cde7c506e65568a7ab4e9a0e
Hinthusiast <68813392+Hinthusiast@users.noreply.github.com>
2021-09-08 09:28:15 +0800

    add MVC framework

--- Testing/SimpleApp/Program.cs
+++ Testing/SimpleApp/Program.cs
@@ -1,11 +1,14 @@
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
+app.UseRouting();
 
 app.MapGet("/", () => "Hello World!");
 
